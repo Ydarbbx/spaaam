108842131c11fe99ab6a3a4449f9cdc5038817f8
ItziiM7mdYT11 <43005364+ItziiM7mdYT11@users.noreply.github.com>
2018-10-24 23:28:53 +0200

    Create bot2.js

---
+++ bot2.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const other = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+other.on('ready', async () => {
+      let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
+  setInterval(() => {
+client.channels.get("504763092614250529").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+},4000);
+});
+
+other.login(process.env.cre1)
