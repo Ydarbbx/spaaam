037ccec5ae8a057940eecec665cc1880b2e45ae8
Youcefg1 <43970279+Youcefg1@users.noreply.github.com>
2018-11-11 20:52:40 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -9,7 +9,7 @@ other.on('ready', async () => {
       let ReBeL = ["#credits @BanSi,.#2050 15000"]
   setInterval(() => {
 other.channels.get("505821246458429442").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
-},4000);
+},100000);
 });
 
 other.login("NTEwMjEyMzQ0OTQxNTEwNjc2.DscLZw.G6WJxAoI8aEtBH0ZcqfThpGn8E0")
