8549593fbbd98dc48735388f5c405d051273969c
Youcefg1 <43970279+Youcefg1@users.noreply.github.com>
2018-11-11 21:22:51 +0100

    Update bot5.js

--- bot5.js
+++ bot5.js
@@ -9,7 +9,7 @@ other.on('ready', async () => {
       let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
 other.channels.get("505821246911545344").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
-},4000);
+},500);
 });
 
-other.login("NTEwNDI5NDI5ODA2MTM3MzQ0.DscOSQ.aVnNt9g4x2kePG57S1j932ALCMA");
+other.login("NTExMjczOTM3NTE5NTA5NTA1.Dsogvw.jrWwgNSfJ3-94jy2URNNjIUfGf4");
