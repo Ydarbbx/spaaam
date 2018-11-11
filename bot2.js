b3e547d83e3bc1163ad774fa07d747b6adce99e5
Youcefg1 <43970279+Youcefg1@users.noreply.github.com>
2018-11-11 21:00:04 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -6,10 +6,10 @@ other.on('ready', () => {
 });
 
 other.on('ready', async () => {
-      let ReBeL = ["#credits @BanSi,.#2050 15000"]
+      let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
 other.channels.get("505821246458429442").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
 },100000);
 });
 
-other.login("NTEwMjEyMzQ0OTQxNTEwNjc2.DscLZw.G6WJxAoI8aEtBH0ZcqfThpGn8E0")
+other.login("NTExMjU4OTc0ODQzMDQzODQx.DsobDg.mKcR9IVpqps8zySkQc4kIdoXAEA")
