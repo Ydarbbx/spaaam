08648e8ab70161d4584cee4a6f6c618020dc1345
Youcefg1 <43970279+Youcefg1@users.noreply.github.com>
2018-11-30 15:37:51 +0100

    Update bot8.js

--- bot8.js
+++ bot8.js
@@ -1,6 +1,16 @@
 const Discord = require('discord.js');
 const other = new Discord.Client();
 
+
+
+ other.on('message',function(message) {
+
+  let args = message.content.split(" ").slice(1).join(" ");
+  if(message.content.startsWith("8s")) {
+    if(!args) return;
+    message.channel.send(`${args}`); 
+}
+});
 other.on('ready', () => {
   console.log(`Logged in as ${other.user.tag}!`);
 });
@@ -8,8 +18,8 @@ other.on('ready', () => {
 other.on('ready', async () => {
       let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
-other.channels.get("507278241506131975").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
-},4000);
+other.channels.get("517419390434541600").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+},300);
 });
 
-other.login("NTEwNDM1ODE0NjU2OTY2NjY3.DscUPg.D2wPt7UyRX7CB5Rnh04aaUqNXlQ")
+other.login("NTE4MDMzNDkyNDg0MDMwNDgx.DuLVHQ.dHpbXCsiPDHSlw8dIzC4vI8bX6c");
