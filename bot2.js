b0e81e6850815277e2de103d20b1c60b411c9764
Youcefg1 <43970279+Youcefg1@users.noreply.github.com>
2018-11-30 13:04:04 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -6,7 +6,7 @@ const other = new Discord.Client();
  other.on('message',function(message) {
 
   let args = message.content.split(" ").slice(1).join(" ");
-  if(message.content.startsWith("1s")) {
+  if(message.content.startsWith("2s")) {
     if(!args) return;
     message.channel.send(`${args}`); 
 }
@@ -18,8 +18,8 @@ other.on('ready', () => {
 other.on('ready', async () => {
       let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
-other.channels.get("505821246458429442").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+other.channels.get("517419390434541600").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
 },300);
 });
 
-other.login("NTExMjU4OTc0ODQzMDQzODQx.DtBRVQ.Bez3EpnKj7wWg3oTBxf4fk4Ngl8");
+other.login("NDk3ODMwNTQ5MzE0ODYzMTI0.DuB9WQ.WUw-mnyW0rgSsMZCwHkgdgTO6s4");
